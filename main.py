fca97588208c75ba85a8f2e08a8980342049ac61
Seuz <>
2021-07-04 10:03:42 +0000

    start

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import discord
 import os
+import sqlite3
 
 client = discord.Client()
+conn = sqlite3.connect('data.db')
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS usershard(
+   userid TEXT PRIMARY KEY,
+   strenght INT,
+   stealth INT,
+   speed INT,
+   intel INT
+   );
+""")
+conn.commit()
+cur.execute("""CREATE TABLE IF NOT EXISTS balance(
+   userid TEXT PRIMARY KEY,
+   bal INT
+   );
+""")
+conn.commit()
+
 
 @client.event
 async def on_ready():
@@ -11,6 +31,10 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if (message.channel.name == 'новобранцы'):
+        cur.execute("""INSERT INTO balance(userid, bal) VALUES(?, ?);""", (message.author.id, 0))
+        conn.commit()
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
